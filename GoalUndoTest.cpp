43455270f7f4409843e646360df9e16dc4c8b3cf
Pritesh Jagani <priteshjagani@gmail.com>
2016-05-17 22:54:26 -0700

    Added Test cases

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -18,3 +18,174 @@ TEST(GoalUndoTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+TEST(GoalUndoTest, testsingleoperation)
+{
+	GoalUndo goal;
+	goal.addOperation("Goal-1","Triangle");
+	ASSERT_EQ("Triangle",goal.getOperations());
+}
+
+
+TEST(GoalUndoTest, testmultipleoperation)
+{
+	GoalUndo goal;
+	goal.addOperation("Goal-1","Triangle");
+	goal.addOperation("Square");
+	goal.addOperation("Circle");
+	goal.addOperation("Rect");
+	
+	ASSERT_EQ("Triangle Square Circle Rect",goal.getOperations());
+
+	
+
+}
+
+TEST(GoalUndoTest, ListOfAllOperations)
+{
+	GoalUndo goal;
+	goal.addOperation("Goal-1","Triangle");
+	goal.addOperation("Square");
+	ASSERT_EQ("Triangle Square",goal.getOperations());
+}
+
+
+TEST(GoalUndoTest, TestGetGoal)
+{
+	GoalUndo goal;
+	goal.addOperation("Goal-1","Triangle");
+	
+	ASSERT_EQ("Goal-1",goal.getGoal());
+}
+
+TEST(GoalUndoTest, TestEmptyGetGoal)
+{
+	GoalUndo goal;
+	
+	
+	ASSERT_EQ("",goal.getGoal());
+}
+
+
+TEST(GoalUndoTest, TestUndoGoal)
+{
+	GoalUndo goal;
+	goal.addOperation("Goal-1","Triangle");
+	goal.undoGoal();
+
+	
+	ASSERT_EQ("",goal.getGoal());
+}
+
+
+
+TEST(GoalUndoTest, TestUndoOperation)
+{
+	GoalUndo goal;
+	goal.addOperation("Goal-1","Triangle");
+	goal.undoOperation();
+
+	
+	ASSERT_EQ("",goal.getOperations());
+}
+
+TEST(GoalUndoTest, TestUndoMultipleGoal)
+{
+	GoalUndo goal;
+	goal.addOperation("Goal-1","Triangle");
+	goal.addOperation("Goal-2","Square");
+	goal.addOperation("Goal-3", "Circle");
+
+	goal.undoGoal();
+
+	
+	ASSERT_EQ("Goal-2",goal.getGoal());
+}
+
+
+
+TEST(GoalUndoTest, TestUndoallGoal)
+{
+	GoalUndo goal;
+	goal.addOperation("Goal-1","Triangle");
+	goal.addOperation("Goal-2","Square");
+	goal.addOperation("Goal-3", "Circle");
+
+	goal.undoGoal();
+
+	goal.undoGoal();
+	goal.undoGoal();
+	
+	ASSERT_EQ("",goal.getGoal());
+}
+
+
+TEST(GoalUndoTest, TestUndoMultipleOperations)
+{
+	GoalUndo goal;
+	goal.addOperation("Goal-1","Triangle");
+	goal.addOperation("Square");
+	goal.addOperation("Circle");
+
+	goal.undoOperation();
+
+	goal.undoOperation();
+	//goal.undoGoal();
+	
+	ASSERT_EQ("Triangle",goal.getOperations());
+}
+
+
+TEST(GoalUndoTest, TestUndoAllOperations)
+{
+	GoalUndo goal;
+	goal.addOperation("Goal-1","Triangle");
+	goal.addOperation("Square");
+	goal.addOperation("Circle");
+
+	goal.undoOperation();
+
+	goal.undoOperation();
+	goal.undoOperation();
+	
+	ASSERT_EQ("",goal.getOperations());
+}
+
+
+
+TEST(GoalUndoTest, TestUndoDifferentGoals)
+{
+	GoalUndo goal;
+	goal.addOperation("Goal-1","Triangle");
+	goal.addOperation("Square");
+	goal.addOperation("Circle");
+	goal.addOperation("Goal-2", "Rect");
+
+	goal.undoOperation();
+
+	goal.undoOperation();
+	goal.undoOperation();
+	
+	ASSERT_EQ("Triangle",goal.getOperations());
+}
+
+TEST(GoalUndoTest, TestUndoDifferentOperations)
+{
+	GoalUndo goal;
+	goal.addOperation("Goal-1","Triangle");
+	goal.addOperation("Square");
+	goal.addOperation("Circle");
+	goal.addOperation("Goal-2", "Rect");
+
+	goal.undoOperation();
+	goal.undoGoal();
+
+	goal.undoOperation();
+	goal.undoOperation();
+	goal.undoGoal();
+	
+	ASSERT_EQ("",goal.getOperations());
+	ASSERT_EQ("", goal.getGoal());
+}
+
+
